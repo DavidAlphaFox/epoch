457613435f3d354765af247936d323d47e250327
Erik Stenman <happi@happihacking.com>
2018-12-19 16:54:05 +0100

    Add call handling.

--- apps/aefate/src/aefa_fate.erl
+++ apps/aefate/src/aefa_fate.erl
@@ -6,7 +6,7 @@
 -include("aefa_data.hrl").
 
 -ifdef(TEST).
--define(trace(I,S), S#{trace => [I|get_trace(S)]}).
+-define(trace(I,S), S#{trace => [{I, erlang:process_info(self(), reductions)} |get_trace(S)]}).
 -else.
 -define(trace(I,S), S).
 -endif.
@@ -41,13 +41,35 @@ step([I|Is], EngineState0) ->
     end.
 
 eval(return, EngineState) ->
-    {stop, EngineState};
+    pop_call_stack(EngineState);
+eval({call_local, Function}, EngineState) ->
+    Signature = get_function_signature(Function, EngineState),
+    ok = check_signature(Signature, EngineState),
+    ES2 = push_return_address(EngineState),
+    {jump, 0,  set_current_function(Function, ES2)};
+eval({tailcall_local, Function}, EngineState) ->
+    Signature = get_function_signature(Function, EngineState),
+    ok = check_signature(Signature, EngineState),
+    {jump, 0,  set_current_function(Function, EngineState)};
+eval({call_remote, Contract, Function}, EngineState) ->
+    ES1 = push_return_address(EngineState),
+    ES2 = set_function(Contract, Function, ES1),
+    Signature = get_function_signature(Function, ES2),
+    ok = check_signature(Signature, ES2),
+    {jump, 0, ES2};
+eval({tailcall_remote, Contract, Function}, EngineState) ->
+    ES2 = set_function(Contract, Function, EngineState),
+    Signature = get_function_signature(Function, ES2),
+    ok = check_signature(Signature, ES2),
+    {jump, 0, ES2};
 eval({jump, BB}, EngineState) ->
     {jump, BB, EngineState};
 eval(push_a_0, EngineState) ->
     {next, push_int(?MAKE_FATE_INTEGER(0), EngineState)};
 eval(inc_a_1_a, EngineState) ->
     {next, inc_acc(EngineState)};
+eval({add_a_i_a, X}, EngineState) ->
+    {next, add_acc(X, EngineState)};
 eval(nop, EngineState) ->
     {next, EngineState}.
 
@@ -66,24 +88,37 @@ setup_engine(#{ contract := Contract
     ES3 = push_arguments(Arguments, ES2),
     ES3.
 
-set_function(Contract, Function, #{ chain := Chain} = ES) ->
-    ContractCode = get_contract(Contract, Chain),
-    ES1 = ES#{current_contract => Contract},
-    ES2 = setup_functions(ContractCode, ES1),
-    set_current_function(Function, ES2).
 
-    
+
+set_function(Contract, Function, #{ chain := Chain
+                                  , contracts := Contracts} = ES) ->
+    {ES2, Code} =
+        case maps:get(Contract, Contracts, void) of
+            void ->
+                ContractCode = chain_get_contract(Contract, Chain),
+                {ES#{contracts => maps:put(Contract, ContractCode, Contracts)},
+                 ContractCode};
+            ContractCode ->
+                {ES, ContractCode}
+        end,
+    ES3 = ES2#{current_contract => Contract},
+    ES4 = setup_functions(Code, ES3),
+    set_current_function(Function, ES4).
+
+%get_current_contract(#{current_contract := Contract}) ->
+%    Contract.
 
 setup_functions(ContractCode, ES) ->
     lists:foldl(
-      fun({FunctionName, BBs}, State) ->
-              set_function_code(FunctionName, BBs, State)
+      fun({FunctionName, Signature, BBs}, State) ->
+              set_function_code(FunctionName, Signature, BBs, State)
       end,
       ES,
       ContractCode).
 
 set_current_function(Function, ES) ->
-    ES1 = ES#{current_function => Function},
+    ES1 = ES#{current_function => Function
+             , current_bb => 0},
     BBs = get_function_code(Function, ES1),
     lists:foldl(
       fun({BB, Instructions}, State) ->
@@ -96,17 +131,44 @@ set_instructions(BB, Is, #{bbs := BBs} = ES) ->
     NewBBs = maps:put(BB, Is, BBs),
     maps:put(bbs, NewBBs, ES).
 
-set_function_code(Name, BBs, #{functions := Functions} = ES) ->
-    NewFunctions = maps:put(Name, BBs, Functions),
+set_function_code(Name, Signature, BBs, #{functions := Functions} = ES) ->
+    NewFunctions = maps:put(Name, {Signature, BBs}, Functions),
     maps:put(functions, NewFunctions, ES).
 
 get_function_code(Name, #{functions := Functions}) ->
     case maps:get(Name, Functions, void) of
         void ->  throw({error, {trying_to_call_function, Name}});
-        Code -> Code
+        {_Signature, Code} -> Code
     end.
-             
-             
+
+get_function_signature(Name,  #{functions := Functions}) ->
+    case maps:get(Name, Functions, void) of
+        void ->  throw({error, {trying_to_call_function, Name}});
+        {Signature, _Code} -> Signature
+    end.
+
+check_signature({ArgTypes, _RetSignature},
+                #{ accumulator := Acc
+                 , accumulator_stack := Stack}
+               =_EngineState) ->
+    case check_arg_types(ArgTypes, [Acc, Stack]) of
+        ok -> ok;
+        {error, T, V}  ->
+            throw({error, {value_does_not_match_type, V, T}})
+    end.
+
+check_arg_types([], _) -> ok;
+check_arg_types([T|Ts], [A|As]) ->
+    case check_type(T,A) of
+        true -> check_arg_types(Ts, As);
+        false -> {error, T, A}
+    end.
+
+check_type(integer, I) when ?IS_FATE_INTEGER(I) -> true;
+check_type({list,_ET}, L) when ?IS_FATE_LIST(L) -> true; %% TODO: check element type
+check_type(T, V) -> {error, T, V}.
+
+
 jump(BB, ES) ->
     NewES = set_current_bb_index(BB, ES),
     Instructions = current_bb(NewES),
@@ -125,9 +187,14 @@ push_arguments(Args, #{ accumulator := X
 push_arguments([], Acc, Stack, ES) ->
     ES#{ accumulator := Acc
        , accumulator_stack := Stack};
-push_arguments([A|As], Acc, Stack, ES ) -> 
+push_arguments([A|As], Acc, Stack, ES ) ->
     push_arguments(As, A, [Acc, Stack], ES).
-     
+
+add_acc(X, #{accumulator := Y} = ES) when ?IS_FATE_INTEGER(X)
+                                          , ?IS_FATE_INTEGER(X) ->
+    ES#{accumulator := ?MAKE_FATE_INTEGER(?FATE_INTEGER_VALUE(X)
+                                          + ?FATE_INTEGER_VALUE(Y)
+                                         )}.
 
 inc_acc(#{accumulator := X} = ES) when ?IS_FATE_INTEGER(X) ->
     ES#{accumulator := ?MAKE_FATE_INTEGER(?FATE_INTEGER_VALUE(X)+1)}.
@@ -143,7 +210,6 @@ get_bb(BB, #{bbs := BBS}) ->
         void -> throw({error, {trying_to_reach_bb, BB}});
         Instrucitions -> Instrucitions
     end.
-            
 
 get_trace(#{trace := T}) -> T.
 
@@ -151,6 +217,28 @@ next_bb_index(#{ current_bb := BB}) ->
     %% TODO check BB + 1 exists.
     BB + 1.
 
+push_return_address(#{ current_bb := BB
+                     , current_function := Function
+                     , current_contract := Contract
+                     , call_stack := Stack } = ES) ->
+    ES#{ call_stack => [{Contract, Function, BB+1}|Stack]}.
+
+pop_call_stack(#{ call_stack := []} = ES) -> {stop, ES};
+pop_call_stack(#{ call_stack := [{Contract, Function, BB}| Rest]
+                , current_contract := CurrentContract
+                , current_function := CurrentFunction} = ES) ->
+    ES1 = ES#{ call_stack => Rest},
+    if CurrentContract =:= Contract -> %% Local return
+            if CurrentFunction =:= Function -> % self return
+                    {jump, BB, ES1};
+               true -> %% Other function same contract
+                    ES2 = set_current_function(Function, ES1),
+                    {jump, BB, ES2}
+            end;
+       true -> %% Non local return
+            ES2 = set_function(Contract, Function, ES1),
+            {jump, BB, ES2}
+    end.
 
 
 new_engine_state(Chain) ->
@@ -161,17 +249,15 @@ new_engine_state(Chain) ->
      , trace => []
      , accumulator => ?FATE_VOID
      , accumulator_stack => []
-     , functions =>  #{}
+     , functions =>  #{} %% Cashe for current contract.
+     , contracts => #{} %% Cashe for loaded contracts.
      , current_contract => ?FATE_VOID
      , current_function => ?FATE_VOID
+     , call_stack => []
      }.
-             
-
 
 %% ----------------------------
 
 %% TODO: real chain interface
-get_contract(ContractAddress, #{ contracts :=  Contracts} = _Chain) ->
+chain_get_contract(ContractAddress, #{ contracts :=  Contracts} = _Chain) ->
     maps:get(ContractAddress, Contracts).
-
-    
--- apps/aefate/test/aefate_engine_test.erl
+++ apps/aefate/test/aefate_engine_test.erl
@@ -9,19 +9,40 @@
 -include_lib("eunit/include/eunit.hrl").
 
 excute_test() ->
+    Chain = setup_chain(),
     #{accumulator := 42} =
         aefa_fate:run(
           make_call(<<"test">>, <<"id">>, [42]),
-          setup_chain()),
+          Chain),
 
     Res2 = aefa_fate:run(
              make_call(<<"test">>, <<"jumps">>, []),
-             setup_chain()),
+             Chain),
     io:format("Res2 ~p~n", [Res2]),
     #{accumulator := 2} =
         aefa_fate:run(
-          make_call(<<"test">>, <<"inc">>, [1]),
-          setup_chain()),
+          make_call(<<"test">>, <<"inc">>, [0]),
+          Chain),
+    #{accumulator := 4} =
+        aefa_fate:run(
+          make_call(<<"test">>, <<"call">>, [0]),
+          Chain),
+    #{accumulator := 3} =
+        aefa_fate:run(
+          make_call(<<"test">>, <<"tailcall">>, [0]),
+          Chain),
+    #{accumulator := 6} =
+        aefa_fate:run(
+          make_call(<<"remote">>, <<"add_five">>, [1]),
+          Chain),
+    #{accumulator := 10} =
+        aefa_fate:run(
+          make_call(<<"test">>, <<"remote_call">>, [4]),
+          Chain),
+    #{accumulator := 9} =
+        aefa_fate:run(
+          make_call(<<"test">>, <<"remote_tailcall">>, [4]),
+          Chain),
     ok.
 
 make_call(Contract, Function, Arguments) ->
@@ -35,24 +56,54 @@ setup_chain() ->
 
 
 setup_contracts() ->
-    #{ <<"test">> => [ {<<"id">>, [{0, [return]}]}
-                     , {<<"jumps">>,
-                        [{0, [ nop
-                             , {jump, 3}]}
-                        ,{1, [ nop ]}
-                        ,{2, [ nop
-                             , return]}
-                       , {3, [ nop
-                             , {jump, 1}]}]
-                       }
-                     , {<<"inc">>,
-                        [{0, [ push_a_0
-                             , inc_a_1_a
-                             , inc_a_1_a
-                             , return
-                             ]}]
-                       }
-                     ]
+    #{ <<"test">> =>
+           [ {<<"id">>, {[integer], integer}, [{0, [return]}]}
+           , {<<"jumps">>, {[], integer},
+              [{0, [ push_a_0
+                   , {jump, 3}]}
+              ,{1, [ nop ]}
+              ,{2, [ nop
+                   , return]}
+              , {3, [ nop
+                    , {jump, 1}]}]
+             }
+           , {<<"inc">>, {[integer],integer},
+              [{0, [ inc_a_1_a
+                   , inc_a_1_a
+                   , return
+                   ]}]
+             }
+           , {<<"call">>, {[integer],integer},
+              [{0, [ inc_a_1_a
+                   , {call_local, <<"inc">>}]}
+              ,{1, [ inc_a_1_a
+                   , return]}
+              ]
+             }
+           , {<<"tailcall">>, {[integer],integer},
+              [{0, [ inc_a_1_a
+                   , {tailcall_local, <<"inc">>}]}
+              ]
+             }
+           , { <<"remote_call">>
+             , {[integer],integer}
+             , [ {0, [ {call_remote, <<"remote">>, <<"add_five">>} ]}
+               , {1, [ inc_a_1_a,
+                       return]}
+               ]
+             }
+           , { <<"remote_tailcall">>
+             , {[integer],integer}
+             , [ {0, [ {tailcall_remote, <<"remote">>, <<"add_five">>} ]}
+               ]
+             }
+           ]
+     , <<"remote">> =>
+           [ {<<"add_five">>, {[integer], integer},
+              [{0, [{add_a_i_a, 5}
+                    , return]}]
+             }
+           ]
        }.
 
                        
